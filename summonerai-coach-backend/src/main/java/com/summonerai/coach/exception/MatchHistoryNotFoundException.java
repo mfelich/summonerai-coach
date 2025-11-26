3b4da331576c3e433bd2b75a22133ac1f14a72db
mfelich <mfelich77@gmail.com>
2025-11-26 21:03:00 +0100

    fix custom exceptions

--- summonerai-coach-backend/src/main/java/com/summonerai/coach/exception/MatchHistoryNotFoundException.java
+++ summonerai-coach-backend/src/main/java/com/summonerai/coach/exception/MatchHistoryNotFoundException.java
@@ -1,9 +1,5 @@
 package com.summonerai.coach.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class MatchHistoryNotFoundException extends RuntimeException {
     public MatchHistoryNotFoundException(String message) {
         super(message);
--- summonerai-coach-backend/src/main/java/com/summonerai/coach/exception/MatchStatsNotFoundException.java
+++ summonerai-coach-backend/src/main/java/com/summonerai/coach/exception/MatchStatsNotFoundException.java
@@ -1,9 +1,5 @@
 package com.summonerai.coach.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class MatchStatsNotFoundException extends RuntimeException {
     public MatchStatsNotFoundException(String message) {
         super(message);
